e2f95683e155b161d4a5a156c25dc99e56c1585d
huseyin <huseyinyavuz@gmail.com>
2023-07-28 10:49:28 +0300

    request body

---
+++ Request_Body.py
@@ -0,0 +1,22 @@
+"""Request Body¶
+When you need to send data from a client (let's say, a browser) to your API, you send it as a request body.
+
+A request body is data sent by the client to your API. A response body is the data your API sends to the client."""
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
