1694de5b1369e7bf64dd6f64ba09424140141a4d
huseyin <huseyinyavuz@gmail.com>
2023-07-29 09:24:48 +0300

    add numbers

--- 7_Request_Body.py
+++ 7_Request_Body.py
@@ -1,4 +1,6 @@
-"""Request Body¶
+"""
+
+Request Body¶
 When you need to send data from a client (let's say, a browser) to your API, you send it as a request body.
 
 A request body is data sent by the client to your API. A response body is the data your API sends to the client."""
@@ -19,13 +21,3 @@ app = FastAPI()
 @app.post("/items/")
 async def create_item(item: Item):
     return item
-
-
-
-
-
-
-
-
-
--
